cf3394bf0e43c8d26064a76b67df256913dbca8e
Peripotec <david.marcelo.maidana@gmail.com>
2023-11-15 08:59:41 -0300

    Camios zhone

--- zhone.js
+++ zhone.js
@@ -257,76 +257,7 @@ pppoe 1 nat enable user ${cuenta}-${cliente}@${localidad}${esviejo} password ${p
 ip-service-map 1 host 1\n
 exit\n
 exit\n`;
-  
-	// Comando para aprovisionar ONU en Bridge Función: Visualizar
-	const AprovisionarBridgeVisual = `configure terminal<br>
-interface gpon-olt_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span><br>
-onu <span class="variable-highlight">${puertoLogico}</span> type <span class="variable-highlight">${tipoONU}</span> sn <span class="variable-highlight">${numeroSerie}</span><br>
-exit<br><br>
-<b>interface gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br></b>
-sn-bind enable sn<br>
-tcont 1 name 1 profile 1G<br>
-tcont 2 name 2 profile 1G<br>
-gemport 1 tcont 1<br>
-gemport 2 tcont 2<br>
-switchport mode hybrid vport 1<br>
-switchport mode hybrid vport 2<br>
-service-port 1 vport 1 user-vlan <span class="variable-highlight">${vlan}</span> user-vlan <span class="variable-highlight">${vlan}</span><br>
-service-port 2 vport 2 user-vlan 141 vlan 141<br>
-dhcpv4-l2-relay-agent enable vport 2<br>
-pppoe-intermediate-agent enable vport 1<br>
-exit<br><br>
-<b>pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br></b>
-service ppp gemport 1 iphost 1 vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/1 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/2 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/3 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/4 mode tag vlan <span class="variable-highlight">${vlan}</span><br><br>
-security-mgmt 1 state enable ingress-type lan protocol web ftp telnet<br>
-security-mgmt 1 start-src-ip 192.168.1.2 end-src-ip 192.168.1.254<br>
-security-mgmt 2 state enable mode forward ingress-type iphost 1 protocol web<br>
-security-mgmt 2 start-src-ip 200.2.127.149 end-src-ip 200.2.127.149<br>
-security-mgmt 3 state enable mode forward ingress-type iphost 1 protocol web<br>
-security-mgmt 3 start-src-ip 200.2.126.34 end-src-ip 200.2.126.34<br>
-ip-service-map 1 host 1<br><br>
-exit<br>
-exit<br>`;
-  
-	// Comando para aprovisionar ONU en Bridge Función: copiar
-	const AprovisionarBridgeCopiar = `configure terminal\n
-interface gpon-olt_1/${placa}/${puerto}\n
-onu ${puertoLogico} type ${tipoONU} sn ${numeroSerie}\n
-exit\n
-interface gpon-onu_1/${placa}/${puerto}/${puertoLogico}\n
-sn-bind enable sn\n
-tcont 1 name 1 profile 1G\n
-tcont 2 name 2 profile 1G\n
-gemport 1 tcont 1\n
-gemport 2 tcont 2\n
-switchport mode hybrid vport 1\n
-switchport mode hybrid vport 2\n
-service-port 1 vport 1 user-vlan ${vlan} vlan ${vlan}\n
-service-port 2 vport 2 user-vlan 141 vlan 141\n
-dhcpv4-l2-relay-agent enable vport 2\n
-pppoe-intermediate-agent enable vport 1\n
-exit\n
-pon-onu-mng gpon-onu_1/${placa}/${puerto}/${puertoLogico}\n
-service ppp gemport 1 iphost 1 vlan ${vlan}\n
-vlan port eth_0/1 mode tag vlan ${vlan}\n
-vlan port eth_0/2 mode tag vlan ${vlan}\n
-vlan port eth_0/3 mode tag vlan ${vlan}\n
-vlan port eth_0/4 mode tag vlan ${vlan}\n
-security-mgmt 1 state enable ingress-type lan protocol web ftp telnet\n
-security-mgmt 1 start-src-ip 192.168.1.2 end-src-ip 192.168.1.254\n
-security-mgmt 2 state enable mode forward ingress-type iphost 1 protocol web\n
-security-mgmt 2 start-src-ip 200.2.127.149 end-src-ip 200.2.127.149\n
-security-mgmt 3 state enable mode forward ingress-type iphost 1 protocol web\n
-security-mgmt 3 start-src-ip 200.2.126.34 end-src-ip 200.2.126.34\n
-ip-service-map 1 host 1\n
-exit\n
-exit\n`;
-  
-  
+ 
 	// Comando para aprovisionar la Telefonía Función: Visualizar
 	const AprovisionarTelefoniaVisual = `configure terminal<br>
 pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
@@ -356,11 +287,6 @@ exit\n`;
 		descripcion: "Aprovisionar ONU con PPPoE",
 		comando: AprovisionarPPPoEVisual, // Utilizamos el comando con <br> para la visualización
 		copiarComando: AprovisionarPPPoECopiar, // Usamos el copiarComando con \n para copiar
-	  },
-	  {
-		descripcion: "Aprovisionar ONU en Bridge",
-		comando: AprovisionarBridgeVisual, // Utilizamos el comando con <br> para la visualización
-		copiarComando: AprovisionarBridgeCopiar, // Usamos el copiarComando con \n para copiar
 	  },
 	];
   
