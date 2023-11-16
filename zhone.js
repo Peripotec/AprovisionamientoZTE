79858112d01f82422a3df29ab158d64ca0110063
Peripotec <david.marcelo.maidana@gmail.com>
2023-11-16 15:39:00 -0300

    test

--- zhone.js
+++ zhone.js
@@ -1,3 +1,6 @@
+import { aprovisionamiento } from './ZTEG-F660.js';
+
+
 //Función que permite copiar los comandos modificados.
 function copiarComando(comando) {
 	const tempTextArea = document.createElement("textarea");
