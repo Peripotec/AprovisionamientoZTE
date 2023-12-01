880a442506395cd7dd0a04b02e3509322f460de8
Peripotec <david.marcelo.maidana@gmail.com>
2023-12-01 12:04:43 -0300

    Agregar comando para sólo provisionar telefonía

--- script.js
+++ script.js
@@ -389,6 +389,61 @@ security-mgmt 3 start-src-ip 200.2.126.34 end-src-ip 200.2.126.34\n
 ip-service-map 1 host 1\n
 exit\n
 exit\n`;
+
+	// Comando para aprovisionar ONU con SÓLO TELEFONÍA Función: Visualizar
+	const AprovisionarSoloTelefonoiaVisual = `configure terminal<br>
+interface gpon-olt_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span><br>
+onu <span class="variable-highlight">${puertoLogico}</span> type <span class="variable-highlight">${tipoONU}</span> sn <span class="variable-highlight">${numeroSerie}</span><br>
+exit<br><br>
+<b>interface gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br></b>
+sn-bind enable sn<br>
+tcont 2 name 2 profile 1G<br>
+gemport 2 tcont 2<br>
+switchport mode hybrid vport 2<br>
+service-port 2 vport 2 user-vlan 141 vlan 141<br>
+dhcpv4-l2-relay-agent enable vport 2<br>
+pppoe-intermediate-agent enable vport 1<br>
+exit<br><br>
+<b>pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br></b>
+service voip gemport 2 vlan 141<br>
+sip-service pots_0/1 profile denwaSIP userid 54<span class="variable-highlight">${caracteristica}</span><span class="variable-highlight">${telefono}</span> username 54<span class="variable-highlight">${caracteristica}</span><span class="variable-highlight">${telefono} password <span class="variable-highlight">${cuentaFormateada}</span></span><span class="variable-highlight">${telefono}</span></span><br>
+security-mgmt 1 state enable ingress-type lan protocol web ftp telnet<br>
+security-mgmt 1 start-src-ip 192.168.1.2 end-src-ip 192.168.1.254<br>
+security-mgmt 2 state enable mode forward ingress-type iphost 1 protocol web<br>
+security-mgmt 2 start-src-ip 200.2.127.149 end-src-ip 200.2.127.149<br>
+security-mgmt 3 state enable mode forward ingress-type iphost 1 protocol web<br>
+security-mgmt 3 start-src-ip 200.2.126.34 end-src-ip 200.2.126.34<br>
+ip-service-map 1 host 1<br><br>
+exit<br>
+exit<br>`;
+  
+	// Comando para aprovisionar ONU con SÓLO TELEFONÍA Función: copiar
+	const AprovisionarSoloTelefonoiaCopiar = `configure terminal\n
+interface gpon-olt_1/${placa}/${puerto}\n
+onu ${puertoLogico} type ${tipoONU} sn ${numeroSerie}\n
+exit\n
+interface gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
+sn-bind enable sn\n
+tcont 2 name 2 profile 1G\n
+gemport 2 tcont 2\n
+switchport mode hybrid vport 2\n
+service-port 2 vport 2 user-vlan 141 vlan 141\n
+dhcpv4-l2-relay-agent enable vport 2\n
+exit\n
+pon-onu-mng gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
+service voip gemport 2 vlan 141\n
+sip-service pots_0/1 profile denwaSIP userid 54${caracteristica}${telefono} username 54${caracteristica}${telefono} password ${cuentaFormateada}${telefono}\n
+security-mgmt 1 state enable ingress-type lan protocol web ftp telnet\n
+security-mgmt 1 start-src-ip 192.168.1.2 end-src-ip 192.168.1.254\n
+security-mgmt 2 state enable mode forward ingress-type iphost 1 protocol web\n
+security-mgmt 2 start-src-ip 200.2.127.149 end-src-ip 200.2.127.149\n
+security-mgmt 3 state enable mode forward ingress-type iphost 1 protocol web\n
+security-mgmt 3 start-src-ip 200.2.126.34 end-src-ip 200.2.126.34\n
+ip-service-map 1 host 1\n
+exit\n
+exit\n`;
+
+
   
 	// Comando para aprovisionar ONU en Trunk Función: Visualizar
 	const AprovisionarenTrunkVisual = `configure terminal<br>
@@ -464,6 +519,11 @@ exit\n`;
 		descripcion: "Aprovisionar ONU en Bridge",
 		comando: AprovisionarBridgeVisual, // Utilizamos el comando con <br> para la visualización
 		copiarComando: AprovisionarBridgeCopiar, // Usamos el copiarComando con \n para copiar
+	  },
+	  {
+		descripcion: "Aprovisionar ONU con SÓLO TELEFONÍA",
+		comando: AprovisionarSoloTelefonoiaVisual, // Utilizamos el comando con <br> para la visualización
+		copiarComando: AprovisionarSoloTelefonoiaCopiar, // Usamos el copiarComando con \n para copiar
 	  },
 	  {
 		descripcion: "Aprovisionar ONU en Trunk",
