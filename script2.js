5d8fbcbe8c2b981fb5587bb082fb7f5874533049
Peripotec <david.marcelo.maidana@gmail.com>
2023-12-13 11:05:26 -0300

    Testeo correcciones

--- script2.js
+++ script2.js
@@ -10,12 +10,23 @@ function cargarScript() {
         existingScript.remove();
     }
 
-    // Crea y carga el nuevo script
+    // Crea y carga el nuevo script de forma asincrónica
     const newScript = document.createElement("script");
     newScript.id = "onu-script";
     newScript.type = "module";
     const scriptPath = `js/${selectedValue}.js`; // Ruta relativa a la carpeta que contiene 'js'
     console.log("Script Path:", scriptPath);
     newScript.src = scriptPath;
+    newScript.onload = () => {
+        // Una vez que el script se ha cargado correctamente, habilita las funciones
+        habilitarFunciones();
+    };
     document.head.appendChild(newScript);
+}
+
+function habilitarFunciones() {
+    // Habilita las funciones una vez que el script se ha cargado
+    document.getElementById("comandos-btn").onclick = comandos;
+    document.getElementById("aprovisionamiento-btn").onclick = aprovisionamiento;
+    document.getElementById("modificaciones-btn").onclick = modificaciones;
 }
