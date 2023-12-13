900d7f284f09beafa1c310745a1acac4b0a66a9c
Peripotec <david.marcelo.maidana@gmail.com>
2023-12-13 10:59:11 -0300

    Test correcciones...

--- script2.js
+++ script2.js
@@ -1,20 +1,21 @@
 // script2.js
 
 function cargarScript() {
-	const select = document.getElementById("tipo-onu");
-	const selectedValue = select.options[select.selectedIndex].value;
-  
-	// Elimina el script anterior si existe
-	const existingScript = document.getElementById("onu-script");
-	if (existingScript) {
-	  existingScript.remove();
-	}
-  
-	// Crea y carga el nuevo script
-	const newScript = document.createElement("script");
-	newScript.id = "onu-script";
-	newScript.type = "module";
-	newScript.src = `./js/${selectedValue}.js`; // Ruta relativa a la carpeta 'js'
-	document.head.appendChild(newScript);
-  }
-  
+    const select = document.getElementById("tipo-onu");
+    const selectedValue = select.options[select.selectedIndex].value;
+
+    // Elimina el script anterior si existe
+    const existingScript = document.getElementById("onu-script");
+    if (existingScript) {
+        existingScript.remove();
+    }
+
+    // Crea y carga el nuevo script
+    const newScript = document.createElement("script");
+    newScript.id = "onu-script";
+    newScript.type = "module";
+    const scriptPath = `js/${selectedValue}.js`; // Ruta relativa a la carpeta que contiene 'js'
+    console.log("Script Path:", scriptPath);
+    newScript.src = scriptPath;
+    document.head.appendChild(newScript);
+}
