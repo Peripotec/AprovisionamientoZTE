9ff34acbfc333b0e43cd4e765f8605158461666e
Peripotec <david.marcelo.maidana@gmail.com>
2024-04-22 12:41:56 -0300

    Correcciones ONUs Oggier

--- js/ZTE-F601.Oggier.js
+++ js/ZTE-F601.Oggier.js
@@ -5,8 +5,7 @@
 	const puertoLogico = document.getElementById("puerto-logico").value || "x"; // Agregar 'x' si está vacío
 	const macInput = document.getElementById("mac").value;
 	const mac = formatearDireccionMAC(macInput) || "xxxx.xxxx.xxxx"; // Formatear la dirección MAC
-	const numeroSerie =
-	document.getElementById("no-serie").value || "ZTEGCXXXXXXX"; // Agregar 'x' si está vacío
+	const numeroSerie =	document.getElementById("no-serie").value || "ZTEGCXXXXXXX"; // Agregar 'x' si está vacío
   
 	// Función para formatear la dirección MAC
 	function formatearDireccionMAC(mac) {
@@ -49,10 +48,6 @@
 		comando: `show gpon remote-onu interface eth gpon-onu_1/${placa}/${puerto}:${puertoLogico}`,
 	  },
 	  {
-		descripcion: "Ver estado de la Telefonía",
-		comando: `show gpon remote-onu interface pots gpon-onu_1/${placa}/${puerto}:${puertoLogico}`,
-	  },
-	  {
 		descripcion: "Visualizar ONUs asignadas en una placa/puerto",
 		comando: `show running-config interface gpon-olt_1/${placa}/${puerto}`,
 	  },
@@ -85,108 +80,56 @@
 	const tipoONU = document.getElementById("tipo-onu").value || "ZTEX-FXXX"; // Agregar 'ZTEX-FXXX' si está vacío
 	const numeroSerie =	document.getElementById("no-serie").value || "ZTEGCXXXXXXX"; // Agregar 'ZTEGCXXXXXXX' si está vacío
 	const { vlan } = caracteristicaylocalidades(); // Asignar el valor VLAN
-	const { caracteristica } = caracteristicaylocalidades(); // Asignar el valor Caracterísitca
-	const telefono = document.getElementById("telefono").value || "XXXXXX"; // Agregar 'x' si está vacío
-	const cuentaFormateada = formatearCuenta();
 	const cuenta = document.getElementById("cuenta").value || "cuenta"; // Agregar 'CUENTA' si está vacío
 	const cliente = document.getElementById("cliente").value || "cliente"; // Agregar 'CLIENTE' si está vacío
 	const pppoe = document.getElementById("clave-pppoe").value || "AAA000AA"; // Agregar 'AAA000AA' si está vacío
 	const localidad = document.getElementById("localidad").value || "Localidad"; // Agrega 'Localidad' si está vacío
 	const esviejoCheckbox = document.getElementById("esviejo"); // Comprueba si el checkbox está marcado
 	const esviejo = esviejoCheckbox.checked ? "-wilnet" : ""; // Le asigna un valor, si es true le asigna ''
-	const pots = document.getElementById("pots"); // Comprueba si el checkbox está marcado
-	const numpots = pots.checked ? "2" : "1"; // Le asigna por defecto el valor a pots 1, si es true le asigna '2'
 	const vlanInput = document.getElementById("vlan").value || "XXX"; // Obtener los valores de las vlans para el aprovisionamiento en Trunk
 	const { vlan1, vlan2, vlan3, vlan4 } = separarVLANs(vlanInput); // Guardo los valores individuales para asignar vlans trunkeables en cada puerto.
 	
-	// Comando para aprovisionar ONU con PPPoE Función: Visualizar
-	const AprovisionarPPPoEVisual = `configure terminal<br>
-interface gpon-olt_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span><br>
-onu <span class="variable-highlight">${puertoLogico}</span> type <span class="variable-highlight">ZTEG-F670</span> sn <span class="variable-highlight">${numeroSerie}</span><br>
-exit<br><br>
-interface gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
-tcont 1 name 1 profile 1G<br>
-gemport 1 unicast tcont 1 dir both<br>
-service-port 1 vport 1 user-vlan ${vlan} user-etype PPPOE vlan ${vlan}<br>
-pppoe-plus enable vport 1<br>
-exit<br><br>
-pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
-service ppp type internet gemport 1 vlan ${vlan}<br>
-weight tcont 1 queue 1 0<br>
-ip-host 1 id ppp<br>
-pppoe 1 nat enable user ${cuenta}-${cliente}@${localidad}${esviejo} password ${pppoe}<br>
-ip-service-map 1 host 1<br>
-exit<br>
-exit<br>`;
   
-	// Comando para aprovisionar ONU con PPPoE Función: copiar
-	const AprovisionarPPPoECopiar = `configure terminal\n
-interface gpon-olt_1/${placa}/${puerto}\n
-onu ${puertoLogico} type ZTEG-F670 sn ${numeroSerie}\n
-exit\n
-interface gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-tcont 1 name 1 profile 1G\n
-gemport 1 unicast tcont 1 dir both\n
-service-port 1 vport 1 user-vlan ${vlan} user-etype PPPOE vlan ${vlan}\n
-pppoe-plus enable vport 1\n
-exit\n
-pon-onu-mng gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-service ppp type internet gemport 1 vlan ${vlan}\n
-weight tcont 1 queue 1 0\n
-ip-host 1 id ppp\n
-pppoe 1 nat enable user ${cuenta}-${cliente}@${localidad}${esviejo} password ${pppoe}\n
-ip-service-map 1 host 1\n
-exit\n
-exit\n`;
-	
-	
-
 	// Comando para aprovisionar ONU en Bridge Función: Visualizar
 	const AprovisionarBridgeVisual = `configure terminal<br>
 interface gpon-olt_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span><br>
-onu <span class="variable-highlight">${puertoLogico}</span> type <span class="variable-highlight">ZTEG-F670</span> sn <span class="variable-highlight">${numeroSerie}</span><br>
+onu <span class="variable-highlight">${puertoLogico}</span> type <span class="variable-highlight">ZTE-F601</span> sn <span class="variable-highlight">${numeroSerie}</span><br>
 exit<br><br>
 <b>interface gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br></b>
-sn-bind enable sn<br>
-tcont 1 name 1 profile 1G<br>
+tcont 1 name tcont1 profile 1G<br>
 gemport 1 unicast tcont 1 dir both<br>
 switchport mode hybrid vport 1<br>
 service-port 1 vport 1 user-vlan <span class="variable-highlight">${vlan}</span> user-etype PPPOE vlan <span class="variable-highlight">${vlan}</span><br>
 pppoe-plus enable vport 1<br>
 exit<br><br>
 <b>pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br></b>
-service ppp type internet gemport 1 vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/1 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/2 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/3 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/4 mode tag vlan <span class="variable-highlight">${vlan}</span><br><br>
-ip-service-map 1 host 1<br><br>
+service ppp gemport 1 iphost 1 vlan <span class="variable-highlight">${vlan}</span><br>
+vlan port eth_0/1 mode tag vlan <span class="variable-highlight">${vlan}</span><br><br>
 exit<br>
-exit<br>`;
+exit<br><br>
+wr<br>`;
   
 	// Comando para aprovisionar ONU en Bridge Función: copiar
 	const AprovisionarBridgeCopiar = `configure terminal\n
 interface gpon-olt_1/${placa}/${puerto}\n
-onu ${puertoLogico} type ZTEG-F670 sn ${numeroSerie}\n
-exit\n
+onu ${puertoLogico} type ZTE-F601 sn ${numeroSerie}\n
+exit\n\n
 interface gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-sn-bind enable sn\n
-tcont 1 name 1 profile 1G\n
+tcont 1 name tcont1 profile 1G\n
 gemport 1 unicast tcont 1 dir both\n
 switchport mode hybrid vport 1\n
 service-port 1 vport 1 user-vlan ${vlan} user-etype PPPOE vlan ${vlan}\n
 pppoe-plus enable vport 1\n
-exit\n
+exit\n\n
 pon-onu-mng gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-service ppp type internet gemport 1 vlan ${vlan}\n
-vlan port eth_0/1 mode tag vlan ${vlan}\n
-vlan port eth_0/2 mode tag vlan ${vlan}\n
-vlan port eth_0/3 mode tag vlan ${vlan}\n
-vlan port eth_0/4 mode tag vlan ${vlan}\n
-ip-service-map 1 host 1\n
+service ppp gemport 1 iphost 1 vlan ${vlan}\n
+vlan port eth_0/1 mode tag vlan ${vlan}\n\n
 exit\n
-exit\n`;
+exit\n\n
+wr\n`;
 
+    
+  
 	const comandosAprovisionamiento = [
 	  comandosFijos, // Comandos fijos que no se modifican y van al principio
 	  {
@@ -194,15 +137,10 @@ exit\n`;
 		comando: `show running-config interface gpon-olt_1/${placa}/${puerto}`,
 	  },
 	  {
-		descripcion: "Aprovisionar ONU con PPPoE",
-		comando: AprovisionarPPPoEVisual, // Utilizamos el comando con <br> para la visualización
-		copiarComando: AprovisionarPPPoECopiar, // Usamos el copiarComando con \n para copiar
-	  },
-	  {
 		descripcion: "Aprovisionar ONU en Bridge",
 		comando: AprovisionarBridgeVisual, // Utilizamos el comando con <br> para la visualización
 		copiarComando: AprovisionarBridgeCopiar, // Usamos el copiarComando con \n para copiar
-	  },
+	  }
 	];
   
 	mostrarComandos(comandosAprovisionamiento);
@@ -214,19 +152,15 @@ exit\n`;
 	const puerto = document.getElementById("puerto").value || "x"; // Agregar 'x' si está vacío
 	const puertoLogico = document.getElementById("puerto-logico").value || "x"; // Agregar 'x' si está vacío
 	const tipoONU = document.getElementById("tipo-onu").value || "ZTEX-FXXX"; // Agregar 'ZTEX-FXXX' si está vacío
-	const numeroSerie =	document.getElementById("no-serie").value || "ZTEGCXXXXXXX"; // Agregar 'ZTEGCXXXXXXX' si está vacío
+	const numeroSerie =
+	document.getElementById("no-serie").value || "ZTEGCXXXXXXX"; // Agregar 'ZTEGCXXXXXXX' si está vacío
 	const { vlan } = caracteristicaylocalidades(); // Asignar el valor VLAN
-	const { caracteristica } = caracteristicaylocalidades(); // Asignar el valor Caracterísitca
-	const telefono = document.getElementById("telefono").value || "XXXXXX"; // Agregar 'x' si está vacío
-	const cuentaFormateada = formatearCuenta();
 	const cuenta = document.getElementById("cuenta").value || "cuenta"; // Agregar 'CUENTA' si está vacío
 	const cliente = document.getElementById("cliente").value || "cliente"; // Agregar 'CLIENTE' si está vacío
 	const pppoe = document.getElementById("clave-pppoe").value || "AAA000AA"; // Agregar 'AAA000AA' si está vacío
 	const localidad = document.getElementById("localidad").value || "Localidad"; // Agrega 'Localidad' si está vacío
 	const esviejoCheckbox = document.getElementById("esviejo"); // Comprueba si el checkbox está marcado
 	const esviejo = esviejoCheckbox.checked ? "-wilnet" : ""; // Le asigna un valor, si es true le asigna ''
-	const pots = document.getElementById("pots"); // Comprueba si el checkbox está marcado
-	const numpots = pots.checked ? "2" : "1"; // Le asigna por defecto el valor a pots 1, si es true le asigna '2'
   
 	// Comando para Eliminar ONU Función: Visualizar
 	const EliminarONUVisual = `configure terminal<br>
@@ -272,43 +206,6 @@ restore factory\n
 exit\n
 exit\n`;
 
-    	// Comando para desactivar el WiFi de la ONU Función: Visualizar
-	const NoWiFiONUVisual = `configure terminal<br>
-pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
-wifi disable<br>
-exit<br>
-exit<br>`;
-	
-		// Comando para desactivar el WiFi de la ONU Función: Copiar
-	  const NoWiFiONUCopiar = `configure terminal\n
-pon-onu-mng gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-wifi disable\n
-exit\n
-exit\n`;
-  
-	// Comando para cambiar la VLAN (ONU con PPPoE) Función: Visualizar
-	const CambiarVLANconPPPoEVisual = `configure terminal<br>
-interface gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
-no service-port 1<br>
-service-port 1 vport 1 user-vlan <span class="variable-highlight">${vlan}</span> user-etype PPPOE vlan <span class="variable-highlight">${vlan}</span><br>
-exit<br>
-pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
-no service ppp<br>
-service ppp type internet gemport 1 vlan <span class="variable-highlight">${vlan}</span><br>
-exit<br>
-exit<br>`;
-  
-	// Comando para cambiar la VLAN (ONU con PPPoE) Función: Copiar
-	const CambiarVLANconPPPoECopiar = `configure terminal\n
-interface gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-no service-port 1\n
-service-port 1 vport 1 user-vlan ${vlan} user-etype PPPOE vlan ${vlan}\n
-exit\n
-pon-onu-mng gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-no service ppp\n
-service ppp type internet gemport 1 vlan ${vlan}\n
-exit\n
-exit\n`;
   
 	// Comando para cambiar la VLAN (ONU en Bridge) Función: Visualizar
 	const CambiarVLANenBRIDGEVisual = `configure terminal<br>
@@ -318,10 +215,7 @@ service-port 1 vport 1 user-vlan <span class="variable-highlight">${vlan}</span>
 exit<br>
 pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
 no service ppp<br>
-vlan port eth_0/1 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/2 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/3 mode tag vlan <span class="variable-highlight">${vlan}</span><br>
-vlan port eth_0/4 mode tag vlan <span class="variable-highlight">${vlan}</span><br><br>
+vlan port eth_0/1 mode tag vlan <span class="variable-highlight">${vlan}</span><br><br>
 exit<br>
 exit<br>`;
   
@@ -334,43 +228,10 @@ exit\n
 pon-onu-mng gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
 no service ppp\n
 vlan port eth_0/1 mode tag vlan ${vlan}\n
-vlan port eth_0/2 mode tag vlan ${vlan}\n
-vlan port eth_0/3 mode tag vlan ${vlan}\n
-vlan port eth_0/4 mode tag vlan ${vlan}\n
-exit\n
-exit\n`;
-  
-	// Comando para cambiar el PPPoE Función: Visualizar
-	const CambiarPPPoEVisual = `configure terminal<br>
-pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
-no pppoe<br>
-pppoe 1 nat enable user <span class="variable-highlight">${cuenta}-${cliente}@</span><span class="variable-highlight">${localidad}</span><span class="variable-highlight">${esviejo}</span> password <span class="variable-highlight">${pppoe}</span><br>
-exit<br>
-exit<br>`;
-  
-	// Comando para cambiar el PPPoE Función: Copiar
-	const CambiarPPPoECopiar = `configure terminal\n
-pon-onu-mng gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-no pppoe\n
-pppoe 1 nat enable user ${cuenta}-${cliente}@${localidad}${esviejo} password ${pppoe}\n
+
 exit\n
 exit\n`;
  
-	// Comando para cambiar la Telefonía Función: Visualizar
-	const CambiarTelefoniaVisual = `configure terminal<br>
-pon-onu-mng gpon-onu_1/<span class="variable-highlight">${placa}</span>/<span class="variable-highlight">${puerto}</span>:<span class="variable-highlight">${puertoLogico}</span><br>
-no sip-service pots_0/<span class="variable-highlight">${numpots}</span><br>
-sip-service pots_0/<span class="variable-highlight">${numpots}</span> profile denwaSIP userid 54<span class="variable-highlight">${caracteristica}</span><span class="variable-highlight">${telefono}</span> username 54<span class="variable-highlight">${caracteristica}</span><span class="variable-highlight">${telefono}</span> password <span class="variable-highlight">${cuentaFormateada}</span><span class="variable-highlight">${telefono}</span> media-profile wiltelMEDIA<br>
-exit<br>
-exit<br>`;
-  
-	// Comando para cambiar la Telefonía Función: Copiar
-	const CambiarTelefoniaCopiar = `configure terminal\n
-pon-onu-mng gpon-onu_1/${placa}/${puerto}:${puertoLogico}\n
-no sip-service pots_0/${numpots}\n
-sip-service pots_0/${numpots} profile denwaSIP userid 54${caracteristica}${telefono} username 54${caracteristica}${telefono} password ${cuentaFormateada}${telefono} media-profile wiltelMEDIA\n
-exit\n
-exit\n`;
   
 	const comandosModificaciones = [
 	  {
@@ -384,29 +245,9 @@ exit\n`;
 		copiarComando: ResetearONUCopiar, // Usamos el comando con \n para copiar
 	  },
 	  {
-		descripcion: "Cambiar PPPoE en ONU",
-		comando: CambiarPPPoEVisual, // Utilizamos el comando con <br> para la visualización
-		copiarComando: CambiarPPPoECopiar, // Usamos el comando con \n para copiar
-	  },
-	  {
-		descripcion: "Cambiar Telefonía en ONU",
-		comando: CambiarTelefoniaVisual, // Utilizamos el comando con <br> para la visualización
-		copiarComando: CambiarTelefoniaCopiar, // Usamos el comando con \n para copiar
-	  },
-	  {
 		descripcion: "Cambiar VLAN (ONU en Bridge)",
 		comando: CambiarVLANenBRIDGEVisual, // Utilizamos el comando con <br> para la visualización
 		copiarComando: CambiarVLANenBRIDGECopiar, // Usamos el comando con \n para copiar
-	  },
-	  {
-		descripcion: "Cambiar VLAN (ONU con PPPoE)",
-		comando: CambiarVLANconPPPoEVisual, // Utilizamos el comando con <br> para la visualización
-		copiarComando: CambiarVLANconPPPoECopiar, // Usamos el comando con \n para copiar
-	  },
-	  {
-		descripcion: "Desactivar WiFi de la ONU (No funciona en todos los modelos)",
-		comando: NoWiFiONUVisual, // Utilizamos el comando con <br> para la visualización
-		copiarComando: NoWiFiONUCopiar, // Usamos el comando con \n para copiar
 	  },
 	  {
 		descripcion: "Eliminar ONU",
@@ -418,3 +259,4 @@ exit\n`;
   
 	mostrarComandos(comandosModificaciones);
   }
+  
