c1f68eccb94c5c2d30e8518c64560e5880c2cacd
Ricardo Campos <rcampos@wiltel.com.ar>
2026-06-25 15:18:57 -0300

    Update ConfigZTE.js
    Se agrega "ZTE-F6201B" a localidades remotas

--- js/ConfigZTE.js
+++ js/ConfigZTE.js
@@ -25,19 +25,19 @@ export const modelos = {
 export const organizacion = {
     "Wiltel": {
         "rafaela": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
-        "sunchales": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "bellaitalia": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "clucellas": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "esperanza": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "humboldt": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F609V5.2", "ZTE-F6600R"],
-        "humbertoprimo": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "lehmann": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "morteros": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "pilar": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "sancarloscentro": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "sancarlossud": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F601", "ZTE-F6600R"], // F601 repetido intencionalmente si estaba así, o simplificamos. Dejemoslo como estaba.
-        "sanjorge": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
-        "sastre": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R"],
+        "sunchales": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "bellaitalia": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "clucellas": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "esperanza": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "humboldt": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F609V5.2", "ZTE-F6600R", "ZTE-F6201B"],
+        "humbertoprimo": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "lehmann": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "morteros": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "pilar": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "sancarloscentro": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "sancarlossud": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F601", "ZTE-F6600R", "ZTE-F6201B"], // F601 repetido intencionalmente si estaba así, o simplificamos. Dejemoslo como estaba.
+        "sanjorge": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
+        "sastre": ["default", "ZTE-F601", "ZTE-F601C", "ZTEG-F625", "ZTEG-F660", "ZTEG-F668", "ZTEG-F660V8.0", "ZTE-F6600R", "ZTE-F6201B"],
     },
     "Socios": {
         "sancarlosnorte": ["default", "ZTE-F601.OggierSCN", "ZTEG-F660.OggierSCN", "ZTEG-F670.OggierSCN"],
