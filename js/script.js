1e6da773c32a1a1ed558dfd846e480b8867fbf18
Ricardo Campos <rcampos@wiltel.com.ar>
2026-09-10 14:40:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -239,4 +239,8 @@ if (typeof modificaciones === 'function') window.modificaciones = modificaciones
 	// cargarScript();
 });
 
+// Publicar funciones globalmente a window
+window.comandos = comandos;
+window.aprovisionamiento = aprovisionamiento;
+window.modificaciones = modificaciones;
 
